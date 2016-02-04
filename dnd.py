f574dcc53073128adb70ef0e8f437006d3d21b92
Jacob Rogers <self.onyxus@gmail.com>
2016-02-04 10:05:54 -0800

    Completed basic parsing of first layer of the xml (python script generates debug output)

--- dnd.py
+++ dnd.py
@@ -1,8 +1,36 @@
 import xml.etree.ElementTree as ET
+import random
 
 # Import the Race, Class, and Background data
 tree = ET.parse('data.xml')
 root = tree.getroot()
 
+# Arrays for storing and randomizing values.
+all_races = []
+all_jobs = []
+all_backgrounds = []
+
+for child in root:
+    if child.tag == 'race':
+        all_races.append(child)
+
+    elif child.tag == 'class':
+        all_jobs.append(child)
+
+    elif child.tag == 'background':
+        all_backgrounds.append(child)
+
+race = all_races[random.randrange(0, len(all_races))]
+job = all_jobs[random.randrange(0, len(all_jobs))]
+background = all_backgrounds[random.randrange(0, len(all_backgrounds))]
+
+ability_scores = {'str' : random.randrange(3,19), 'dex' : random.randrange(3,19), 'con' : random.randrange(3,19), 'int' : random.randrange(3,19), 'wis' : random.randrange(3,19), 'cha' : random.randrange(3,19)}
+racial_abilities = []
+job_features = []
+background_features = []
 
 
+print "Race: " + race.attrib['name']
+print "Class: " + job.attrib['name']
+print "Background: " + background.attrib['name']
+print "\nAbility Scores:\n\tSTR: " + str(ability_scores['str']) + "\n\tDEX: " + str(ability_scores['dex']) + "\n\tCON: " + str(ability_scores['con']) + "\n\tINT: " + str(ability_scores['int']) + "\n\tWIS: " + str(ability_scores['wis']) + "\n\tCHA: " + str(ability_scores['cha'])

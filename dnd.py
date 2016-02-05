e83cd57b7ddadf5a4385988a99219a5c13171d76
Jacob Rogers <self.onyxus@gmail.com>
2016-02-04 16:49:13 -0800

    Working on Race parsing v0.1

--- dnd.py
+++ dnd.py
@@ -1,6 +1,15 @@
 import xml.etree.ElementTree as ET
 import random
 
+def parse_race(race, local_abs, local_abilities):
+		
+
+def parse_job(job):
+	pass
+
+def parse_background(background):
+	pass
+
 # Import the Race, Class, and Background data
 tree = ET.parse('data.xml')
 root = tree.getroot()
@@ -28,6 +37,55 @@ ability_scores = {'str' : random.randrange(3,19), 'dex' : random.randrange(3,19)
 racial_abilities = []
 job_features = []
 background_features = []
+
+
+# Race Parsing
+for child in race:
+	if child.tag == 'abs':
+		for attribute in child:
+			ability_scores[attribute.tag] += int(attribute.attrib['value'])
+
+	elif child.tag == 'age':
+		low = 0
+		high = 0
+		for number in child:
+			if number.tag == 'high':
+				high = int(number.attrib['value'])
+			elif number.tag == 'low':
+				low = int(number.attrib['value'])
+			else:
+				pass
+		age = random.randrange(low, high+1)
+
+	elif child.tag == 'alignment':
+		for aspect in child:
+			civil = ""
+			moral = ""
+			if aspect.tag == 'civil':
+				civil = aspect.attrib['value']
+			elif aspect.tag == 'moral':
+				moral = aspect.attrib['value']
+			else:
+				pass
+		if civil == "":
+			civilities = ["Lawful", "Neutral", "Chaotic"]
+			civil = civilities[random.randrange(0, len(civilities)]
+		if moral == "":
+			moralities = ["Good", "Neutral", "Evil"]
+			moral = moralities[random.randrange(0, len(moralities)]
+		if civil == "Neutral" and moral == "Neutral":
+			civil = "True"
+		alignment = civil + " " + moral
+
+	elif child.tag == 'size':
+		size = child.attrib['value']
+
+	elif child.tag == 'speed':
+		speed = child.attrib['value']
+
+	elif child.tag == 'ability':
+		abilities = {}
+		for ability in child:
 
 
 print "Race: " + race.attrib['name']
